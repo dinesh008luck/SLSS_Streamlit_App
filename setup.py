9400b896371f3bc7699f7871c11251274c159d05
dinesh008luck <121578314+dinesh008luck@users.noreply.github.com>
2024-12-10 23:32:39 +0530

    add cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Slss Reporting App using Streamlit',
+    author='Dinesh Kumar',
+    license='',
+)
