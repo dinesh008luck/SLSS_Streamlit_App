23af2981aa60920be790798d80b23acbdbc1c62b
dinesh008luck <121578314+dinesh008luck@users.noreply.github.com>
2024-12-11 16:17:36 +0530

    Depandency install

---
+++ src/data/get data.py
@@ -0,0 +1,13 @@
+import pickle
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+from PIL import Image
+import streamlit_authenticator as stauth
+import pyodbc
